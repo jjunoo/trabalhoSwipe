6421ededca5d5b7357bbca332f5e886e01921e81
jjunoo <49094710+jjunoo@users.noreply.github.com>
2019-05-26 20:24:14 -0300

    Initial commit

---
+++ app/src/main/java/com/example/trabalhoswipe/MainActivity.java
@@ -0,0 +1,113 @@
+package com.example.trabalhoswipe;
+
+import android.graphics.drawable.Drawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private LinearLayout layoutContainer;
+    private TextView tvRegiao;
+    private TextView tvUF;
+    private String[] regiao;
+    private String[][] uf;
+    private ImageView [][] ivUF;
+    private int cont, contUF;
+
+    private int [] ivRegiao = new int[] {
+            R.drawable.norte,
+            R.drawable.ne,
+            R.drawable.co,
+            R.drawable.sudeste,
+            R.drawable.sul,
+    };
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        layoutContainer = (LinearLayout) findViewById(R.id.LayoutContainer);
+        tvRegiao =  (TextView) findViewById(R.id.tvRegiao);
+        tvUF = (TextView) findViewById(R.id.tvEstados);
+        contUF = 0;
+        cont = 0;
+
+
+        regiao = new String[] {
+
+                "Norte",
+                "Nordeste",
+                "Centro-Oeste",
+                "Sudeste",
+                "Sul",
+        };
+
+        
+
+        uf = new String[][] {
+                {"Acre",  "Amapá", "Amazonas","Pará"," Rondônia", "Roraima", "Tocantins"},
+                {"Alagoas", "Bahia", "Ceará", "Maranhão", "Paraíba", "Pernambuco", "Piauí", "Rio Grande do Norte", "Sergipe"},
+                {"Distrito Federal", "Goiás", "Mato Grosso", "Mato Grosso do Sul"},
+                {"Espírito Santo", "Minas Gerais", "Rio de Janeiro", "São Paulo"},
+                {"Paraná","Rio Grande do Sul", "Santa Catarina"},
+
+        };
+
+        tvUF.setText(uf[cont][contUF]);
+        tvRegiao.setText( regiao[cont]);
+
+        layoutContainer.setOnTouchListener( new br.pro.adalto.quiztouch.OnSwipeTouchListener(this){
+
+            @Override
+            public void onSwipeTop() {
+                super.onSwipeTop();
+
+                cont ++;
+                if (cont >= regiao.length){
+                    cont = 0;
+                }
+                tvRegiao.setText(regiao[cont]);
+                contUF = 0;
+                tvUF.setText(uf[cont][contUF]);
+            }
+
+            @Override
+            public void onSwipeBottom() {
+                super.onSwipeBottom();
+                cont --;
+                if(cont < 0 ){
+                    cont = regiao.length-1;
+                }
+                tvRegiao.setText( regiao[cont]);
+                contUF = 0;
+                tvUF.setText(uf[cont][contUF]);
+            }
+
+            @Override
+            public void onSwipeRight() {
+                super.onSwipeRight();
+                contUF ++;
+                if (contUF >= uf[cont].length){
+                    contUF = 0;
+                }
+                tvUF.setText(uf[cont][contUF]);
+            }
+
+            @Override
+            public void onSwipeLeft() {
+                super.onSwipeLeft();
+                contUF --;
+                if(contUF < 0 ){
+                    contUF = uf[cont].length-1;
+                }
+                tvUF.setText(uf[cont][contUF]);
+            }
+        });
+    }
+}
